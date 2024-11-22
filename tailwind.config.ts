a759f8ce24eecb86b50c0897843f4206c39843a3
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-11-22 19:35:54 -0300

    build: setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,16 @@ export default {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        headings: "var(--headings)",
+        footer: "var(--footer)",
+        typograph: "var(--typograph)",
+        muted: "var(--muted)",
+        brand: "var(--brand)",
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)"], // Makes Inter the default sans font
+        londrina: ["var(--font-londrina-solid)"],
+        island: ["var(--font-island-moments)"],
       },
     },
   },
